03f07381ab42acacd5447bd0607b1a4b31444160
KongWiki <kongwiki@163.com>
2019-02-24 10:52:44 +0800

    基础-->函数

--- Basis/function/keyParamAndDefault.py
+++ Basis/function/keyParamAndDefault.py
@@ -3,4 +3,18 @@
 @file:   keyParamAndDefault.py
 @time:   19-2-24上午10:09
 @contact: kongwiki@163.com
-""" 
+"""
+
+def printName(firstName, lastName, reverse):
+    if reverse:
+        print(lastName + ', ' + firstName)
+    else:
+        print(firstName, lastName)
+
+
+if __name__ == '__main__':
+    printName('Olga', 'Puchmajerova', False)
+    printName('Olga', 'Puchmajerova', reverse=False)
+    printName('Olga', lastName='Puchmajerova', reverse=False)
+    printName(lastName='Puchmajerova', firstName=' Olga', reverse=False)
+    # printName('Olga', False, lastName='Puchmajerova')
